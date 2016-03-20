4496b2d532afe315ec068ae6d8dff6188dbcb4f0
Liang Wang <ryanrhymes@gmail.com>
2016-03-20 13:55:26 +0000

    coding ...

--- srg-seminar/notifier.ml
+++ srg-seminar/notifier.ml
@@ -65,6 +65,7 @@ let get_talk_page talk_uri =
   Client.get (Uri.of_string talk_uri) >>= fun (resp, body) ->
   Cohttp_lwt_body.to_string body
 
+let re00 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] ("<h1 class='summary'.*>([\s\S]*)</h1>")
 let re01 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] ("<li><img alt=\"User\".*>([\s\S]*)</li>")
 let re02 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] ("<li><img alt=\"Clock\".*>([\s\S]*)</li>")
 let re03 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] "<.*abbr.*>"
@@ -75,22 +76,30 @@ let re07 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] "[\r\n]"
 let re08 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] "<.*>"
 let re09 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] "^\s+"
 let re10 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] "\s+$"
+let re11 = Pcre.regexp ~flags:[`MULTILINE] "\s+"
+let re12 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] "&#8217;"
+
 
 let get_talk_details s = 
+  let x = Pcre.extract_all ~rex:re00 s in
+  let title = Array.get (Array.get x 0) 1 |> (Pcre.replace ~rex:re07) |> (Pcre.replace ~rex:re08) |> (Pcre.replace ~rex:re08) |> (Pcre.replace ~rex:re09) in
   let x = Pcre.extract_all ~rex:re01 s in
-  let speaker = Array.get (Array.get x 0) 1 |> (Pcre.replace ~rex:re07) |> (Pcre.replace ~rex:re08) in
+  let speaker = Array.get (Array.get x 0) 1 |> (Pcre.replace ~rex:re07) |> (Pcre.replace ~rex:re08) |> (Pcre.replace ~rex:re08) |> (Pcre.replace ~rex:re09) in
   let x = Pcre.extract_all ~rex:re02 s in
-  let datetime = Array.get (Array.get x 0) 1 |> Pcre.replace ~rex:re03 in
+  let datetime = Array.get (Array.get x 0) 1 |> Pcre.replace ~rex:re03 |> (Pcre.replace ~rex:re08) |> (Pcre.replace ~rex:re09) in
   let x = Pcre.extract_all ~rex:re04 s in
-  let location = Array.get (Array.get x 0) 1 in
+  let location = Array.get (Array.get x 0) 1 |> (Pcre.replace ~rex:re08) |> (Pcre.replace ~rex:re09) in
   let x = Pcre.extract_all ~rex:re05 s in
   let abstract = Array.get (Array.get x 0) 1 |> (Pcre.extract_all ~rex:re06) |> (Array.map (fun x ->
-    Array.get x 1 |> (Pcre.replace ~rex:re07) |> (Pcre.replace ~rex:re08) |> (Pcre.replace ~rex:re09) |> (Pcre.replace ~rex:re10)
+    Array.get x 1 |> (Pcre.replace ~rex:re07) |> (Pcre.replace ~rex:re08) |> (Pcre.replace ~rex:re09) |> 
+	(Pcre.replace ~rex:re10) |> (Pcre.replace ~rex:re11 ~templ:" ") |> (Pcre.replace ~rex:re12 ~templ:"\'")
   )) |> Array.to_list |> (String.concat "\n\n") in
   let r = "Date: " ^ datetime ^ "\n" ^
     "Location: " ^ location ^ "\n" ^
-    "Speaker: " ^ speaker ^ "\n" ^
-    "" in 
+    "Speaker: " ^ speaker ^ "\n\n" ^
+    "Title: " ^ title ^ "\n\n" ^
+    "Abstact: " ^ abstract ^ "\n\n" ^
+    "SRG Seminar: http://talks.cam.ac.uk/show/index/8316" in
   r
 
 let _ = 
