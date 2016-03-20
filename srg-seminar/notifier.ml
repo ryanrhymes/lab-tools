ada6d9ce4f18bc20cad3d7a1ecbf689dff7fe016
Liang Wang <ryanrhymes@gmail.com>
2016-03-20 12:50:31 +0000

    coding ...

--- srg-seminar/notifier.ml
+++ srg-seminar/notifier.ml
@@ -27,7 +27,7 @@ let get_all_talks s =
 *)
 
 let re0 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] (start_tag ^ "([\s\S]*)" ^ stop_tag)
-let re1 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] ("<h2.*><a.*href=\"(.+)\">([\s\S]*)</a></h2>")
+let re1 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] ("<h2.*><a.*href=\"(.+)\".*>([\s\S]*)</a></h2>")
 let re2 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] ("<p class='details'><img.*>([\s\S]*)</p>")
 let re3 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] ("<p class='location'><img.*><a.*>([\s\S]*)</a>.*</p>")
 let re4 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] ("<p><img alt=\"Clock\".*>([\s\S]*)</p>")
@@ -54,16 +54,39 @@ let get_all_talks s =
 
 let get_all_tkuri s = 
   let ar0 = Pcre.extract_all ~rex:re0 s in
-  let ar1 = Array.map (fun x -> 
+  Array.map (fun x -> 
     let s = Array.get x 1 in 
     let y = Pcre.extract_all ~rex:re1 s in
-    let tkrui = Array.get (Array.get y 0) 1 in
+    Array.get (Array.get y 0) 1
   ) ar0
 
-let get_talk_details talk_uri = 
+let get_talk_page talk_uri = 
   Client.get (Uri.of_string talk_uri) >>= fun (resp, body) ->
   Cohttp_lwt_body.to_string body
 
+let re01 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] ("<li><img alt=\"User\".*>([\s\S]*)</li>")
+let re02 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] ("<li><img alt=\"Clock\".*>([\s\S]*)</li>")
+let re03 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] "<.*abbr.*>"
+let re04 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] ("<li><img alt=\"House\".*><a.*>([\s\S]*)</a>.*</li>")
+let re05 = Pcre.regexp ~flags:[`UNGREEDY; `MULTILINE] ("<p class='urgent'></p>([\s\S]*)<p>This talk is part of.*")
+
+let get_talk_details s = 
+  let x = Pcre.extract_all ~rex:re01 s in
+  let speaker = Array.get (Array.get x 0) 1 in
+  let x = Pcre.extract_all ~rex:re02 s in
+  let datetime = Array.get (Array.get x 0) 1 |> Pcre.replace ~rex:re03 in
+  let x = Pcre.extract_all ~rex:re04 s in
+  let location = Array.get (Array.get x 0) 1 in
+  let x = Pcre.extract_all ~rex:re05 s in
+  let abstract = Array.get (Array.get x 0) 1 in
+  print_endline speaker;
+  print_endline datetime;
+  print_endline location;
+  print_endline abstract;
+  s
+
 let _ = 
-  let s = Lwt_main.run body in
-  get_all_tkuri s |> print_endline;
+  let s = Lwt_main.run body in 
+  let s = Array.get (get_all_tkuri s) 0 in
+  let s = Lwt_main.run (get_talk_page s) in
+  get_talk_details s
